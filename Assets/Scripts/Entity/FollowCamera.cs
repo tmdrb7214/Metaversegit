296e47f255c1ec6d05dc8d9b39847d7c7fddf73d
tmdrb7214 <tmdrb7214@naver.com>
2025-02-18 12:28:06 +0900

    [Feat] 카메라가캐릭터를따라이동 (아직오류있음)

--- Assets/Scripts/Entity/FollowCamera.cs
+++ Assets/Scripts/Entity/FollowCamera.cs
@@ -2,34 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class FollowCamera : MonoBehaviour
 {
     public Transform target;
     float offsetX;
+    float offsetY;
 
-    // Start is called before the first frame update
     void Start()
     {
-        if(target == null)
-        {
-           if(target == null)
-            {
-                return;
+        if (target == null)
+            return;
 
-                offsetX = transform.position.x - target.position.x;
-            }
-        }
+        offsetX = transform.position.x ;
+        offsetY = transform.position.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        if (target == null)
             return;
 
         Vector3 pos = transform.position;
-        pos.x = target.position.x + offsetX;
+        pos.x = target.position.x;
+        pos.y = target.position.y;
         transform.position = pos;
-
     }
 }
