e3c0202b3fde2f011f5373e87d6b7dcf4e900336
tmdrb7214 <tmdrb7214@naver.com>
2025-02-19 15:23:07 +0900

    Npc대화에 선택적용

--- Assets/Scripts/Entity/Npc.cs
+++ Assets/Scripts/Entity/Npc.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Npc : MonoBehaviour
 {
-    public GameObject dialoguPanel;
-    public Text dialogueText;
+    public GameObject dialoguePanel;//대화창
+    public Text dialogueText; //대화내용
 
     private bool isPlayerNear = false;
+    private bool isDialogueActive = false;
+    private int dialogueState = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,15 +24,55 @@ public class Npc : MonoBehaviour
     {
         if(isPlayerNear && Input.GetKeyDown(KeyCode.E))
         {
-            Interact();
+            if (!isDialogueActive)
+            {
+                StartDialogue();
+            }
+            else
+            {
+                EndDialogue();
+            }
+        }
+        
+        if (isDialogueActive)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                ChoiseNumber(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                ChoiseNumber(2);
+            }
         }
     }
-    private void Interact()
+    private void StartDialogue()
     {
-        dialoguPanel.SetActive(true);
-        dialogueText.text = "안녕하세요";
+        dialoguePanel.SetActive(true);
+        isDialogueActive = true;
+        dialogueState = 0;
+        dialogueText.text = "1. 게임하기      2. 아무것도하지않기";
     }
 
+    private void EndDialogue()
+    {
+        dialoguePanel.SetActive(false);
+        isDialogueActive = false;
+    }
+    private void ChoiseNumber(int choice)
+    {
+        switch (choice)
+        {
+            case 1:
+                dialogueText.text = "게임을 시작합니다.";
+                dialogueState = 1;
+                break;
+            case 2:
+                dialogueText.text = "아무것도 하지 않습니다.";
+                dialogueState = 2;
+                break;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -44,6 +87,7 @@ public class Npc : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isPlayerNear = false;
+            dialoguePanel.SetActive(false);
         }
     }
 }
