3e52d0ad3c5b6df6b57c717d4add4bf0c8b0eba8
tmdrb7214 <tmdrb7214@naver.com>
2025-02-19 22:41:30 +0900

    [Feat] 시간구현

--- Assets/Scripts/Flap/GameManager.cs
+++ Assets/Scripts/Flap/GameManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject Bat;
     public GameObject Eagle;
+
+    public Text timeText;
+
+    float time = 0.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +21,8 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        timeText.text = time.ToString("N2");
     }
 
     void MakeEmemy()
